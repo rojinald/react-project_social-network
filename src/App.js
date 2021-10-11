7baa4c55429179de23fd5db2bd5db8130f329a9e
Nikita <rodjinals@gmail.com>
2021-10-11 16:12:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Profile from './components/Profile';
+const App = () => {
+  return (
+    <div className = "app-wrapper" >
+     <Header/>
+     <Navigation/>
+     <Profile/>
+     </div>
+
+
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => {
+    return  <header className ='header'>
+    <img src = 'http://assets.centralindex.com/W/48/db08933953349172c3dea48efd3ce906.png'></img>
+  </header>;
+};
+
+export default Header;
