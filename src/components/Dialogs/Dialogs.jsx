3be0fb7e4d1d5f6d9f193899ef314f01e427ac56
rojinald <rodjinals@gmail.com>
2021-10-25 13:37:07 +0300

    Filling a Messages block

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,29 @@
 import s from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 const Dialogs = (props) => {
     return  <div className = {s.content}>
-        TEST TEXT
+      <div className={s.dialogsItem}>
+          <div className={s.dialog}>
+            <NavLink to = "/dialogs/1">Корги</NavLink>
+              </div>
+          <div className={s.dialog}>
+          <NavLink to = "/dialogs/2">Овчарка</NavLink>
+              </div>
+          <div className={s.dialog}>
+          <NavLink to = "/dialogs/3">Лобрадор</NavLink>
+              </div>
+          <div className={s.dialog}>
+          <NavLink to = "/dialogs/4">Доберман</NavLink>
+              </div>
+          <div className={s.dialog}>
+          <NavLink to = "/dialogs/5">Шпиц</NavLink>
+              </div>
+      </div>
+      <div className={s.dialogsMessages}>
+          <div className={s.message}>Гав гав</div>
+          <div className={s.message}>Хочу кусать себя за ховст</div>
+          <div className={s.message}>Я хочу есть!!</div>
+      </div>
     </div>
 };
 
