154371e8c7d4909bcf054d0530413e4b1fd0e630
rojinald <rodjinals@gmail.com>
2021-11-14 19:20:51 +0300

    refactor state.js , add store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText } from './redux/state';
 
 let reRender = (state) => {
 
    ReactDOM.render(
       <React.StrictMode>
-         <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+         <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
       </React.StrictMode>,
       document.getElementById('root')
    );
@@ -18,9 +17,9 @@ let reRender = (state) => {
 }
 
 
-reRender(state);
+reRender(store.getState());
 
-subscribe(reRender);
+store.subscribe(reRender);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,62 +1,63 @@
-let reRender = () => {
-   console.log('State was changed')
-}
+let store = {
+   _state: {
+      profilePage: {
+         posts: [
+            { id: 1, message: 'Давай дружить!', likesCount: 50 },
+            { id: 2, message: 'Это мой первый пост :)', likesCount: 10 },
+         ],
+         newPostText: 'test'
+      },
+      dialogsPage: {
+         messages: [
+            { id: 1, message: 'Гав Гав!' },
+            { id: 2, message: 'Хочу кусать себя за хвост!' },
+            { id: 3, message: 'Пора грызть тапки' },
+            { id: 4, message: 'Пошли гулять' },
+            { id: 5, message: 'Обними меня' }
+         ],
+         dialogs: [
+            { id: 1, name: 'Корги', img: 'https://porodisobak.ru/wp-content/uploads/2021/07/velsh-korgi-pembrok-11.jpg' },
+            { id: 2, name: 'Овчарка', img: 'https://nekusaka.com/wp-content/uploads/2017/02/german_shepherd14.jpg' },
+            { id: 3, name: 'Лобрадор', img: 'https://38.img.avito.st/image/1/c2sm17ax34IQYF2PTI4wD9h034aadtWA' },
+            { id: 4, name: 'Доберман', img: 'https://sun9-41.userapi.com/impg/n-tSBou1cVzoRXJKjMRf9VElWsnynsTsl1kT7A/e65NXEbZjLQ.jpg?size=604x585&quality=96&sign=7c6849290d6c120693b6882694602e88&type=album' },
+            { id: 5, name: 'Шпиц', img: 'https://sun9-9.userapi.com/impf/Rc_cafcy2i0gvFB_D93VZuJWEQO6kr-KAhp4Zw/uF6JVyDWbCY.jpg?size=604x604&quality=96&sign=dde08087567a35f8cf516e2b14f7775c&type=album' }
+         ],
+      },
+      friendsSideBar: {
+         friends: [
+            { id: 1, name: 'Корги', img: 'https://porodisobak.ru/wp-content/uploads/2021/07/velsh-korgi-pembrok-11.jpg' },
+            { id: 2, name: 'Овчарка', img: 'https://nekusaka.com/wp-content/uploads/2017/02/german_shepherd14.jpg' },
+            { id: 3, name: 'Лобрадор', img: 'https://38.img.avito.st/image/1/c2sm17ax34IQYF2PTI4wD9h034aadtWA' },
+            { id: 4, name: 'Доберман', img: 'https://sun9-41.userapi.com/impg/n-tSBou1cVzoRXJKjMRf9VElWsnynsTsl1kT7A/e65NXEbZjLQ.jpg?size=604x585&quality=96&sign=7c6849290d6c120693b6882694602e88&type=album' },
+            { id: 5, name: 'Шпиц', img: 'https://sun9-9.userapi.com/impf/Rc_cafcy2i0gvFB_D93VZuJWEQO6kr-KAhp4Zw/uF6JVyDWbCY.jpg?size=604x604&quality=96&sign=dde08087567a35f8cf516e2b14f7775c&type=album' }
 
-let state = {
-   profilePage: {
-      posts: [
-         { id: 1, message: 'Давай дружить!', likesCount: 50 },
-         { id: 2, message: 'Это мой первый пост :)', likesCount: 10 },
-      ],
-      newPostText: 'test'
+         ]
+      }
    },
-   dialogsPage: {
-      messages: [
-         { id: 1, message: 'Гав Гав!' },
-         { id: 2, message: 'Хочу кусать себя за хвост!' },
-         { id: 3, message: 'Пора грызть тапки' },
-         { id: 4, message: 'Пошли гулять' },
-         { id: 5, message: 'Обними меня' }
-      ],
-      dialogs: [
-         { id: 1, name: 'Корги', img: 'https://porodisobak.ru/wp-content/uploads/2021/07/velsh-korgi-pembrok-11.jpg' },
-         { id: 2, name: 'Овчарка', img: 'https://nekusaka.com/wp-content/uploads/2017/02/german_shepherd14.jpg' },
-         { id: 3, name: 'Лобрадор', img: 'https://38.img.avito.st/image/1/c2sm17ax34IQYF2PTI4wD9h034aadtWA' },
-         { id: 4, name: 'Доберман', img: 'https://sun9-41.userapi.com/impg/n-tSBou1cVzoRXJKjMRf9VElWsnynsTsl1kT7A/e65NXEbZjLQ.jpg?size=604x585&quality=96&sign=7c6849290d6c120693b6882694602e88&type=album' },
-         { id: 5, name: 'Шпиц', img: 'https://sun9-9.userapi.com/impf/Rc_cafcy2i0gvFB_D93VZuJWEQO6kr-KAhp4Zw/uF6JVyDWbCY.jpg?size=604x604&quality=96&sign=dde08087567a35f8cf516e2b14f7775c&type=album' }
-      ],
+   getState() {
+      return this._state;
    },
-   friendsSideBar: {
-      friends: [
-         { id: 1, name: 'Корги', img: 'https://porodisobak.ru/wp-content/uploads/2021/07/velsh-korgi-pembrok-11.jpg' },
-         { id: 2, name: 'Овчарка', img: 'https://nekusaka.com/wp-content/uploads/2017/02/german_shepherd14.jpg' },
-         { id: 3, name: 'Лобрадор', img: 'https://38.img.avito.st/image/1/c2sm17ax34IQYF2PTI4wD9h034aadtWA' },
-         { id: 4, name: 'Доберман', img: 'https://sun9-41.userapi.com/impg/n-tSBou1cVzoRXJKjMRf9VElWsnynsTsl1kT7A/e65NXEbZjLQ.jpg?size=604x585&quality=96&sign=7c6849290d6c120693b6882694602e88&type=album' },
-         { id: 5, name: 'Шпиц', img: 'https://sun9-9.userapi.com/impf/Rc_cafcy2i0gvFB_D93VZuJWEQO6kr-KAhp4Zw/uF6JVyDWbCY.jpg?size=604x604&quality=96&sign=dde08087567a35f8cf516e2b14f7775c&type=album' }
-
-      ]
+   _callSubsciber() {
+      console.log('State was changed')
+   },
+   addPost() {
+      let newPost = {
+         id: 3,
+         message: this._state.profilePage.newPostText,
+         likesCount: 0
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubsciber(this._state);
+   },
+   updateNewPostText(newText) {
+      this._state.profilePage.newPostText = newText;
+      this._callSubsciber(this._state);
+   },
+   subscribe(observer) {
+      this._callSubsciber = observer;
    }
 }
 
-export let addPost = () => {
-   let newPost = {
-      id: 3,
-      message: state.profilePage.newPostText,
-      likesCount: 0
-   };
-   state.profilePage.posts.push(newPost);
-   state.profilePage.newPostText = '';
-   reRender(state);
-}
-
-export let updateNewPostText = (newText) => {
-   state.profilePage.newPostText = newText;
-   reRender(state);
-}
-
-export let subscribe = (observer) => {
-   reRender = observer;
-}
-
-
-export default state;
+export default store;
+window.store = store;
