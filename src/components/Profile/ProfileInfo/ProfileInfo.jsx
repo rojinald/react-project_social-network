c0b8653ca1850173fa6db55c3483313a94a7c48e
rojinald <rodjinals@gmail.com>
2021-12-21 13:19:43 +0300

    extracting the userId using withRouter

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,13 +10,29 @@ const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
-    debugger;
     return <div>
         <div >
             <img className={s.profileCover} src='https://oboi.ws/wallpapers/28_6867_oboi_travjanoj_zakat_2560x1024.jpg'></img>
         </div>
-        <div className={s.profileAvatar}>
-            <img src={props.profile.photos.large}></img>
+        <div className={s.profileItem}>
+
+            <div className={s.profileAvatar}>
+                <img src={props.profile.photos.large}></img>
+            </div>
+            <div className={s.profileInfo}>
+                <div className={s.profileName}>
+                    {props.profile.fullName}
+                </div>
+                <div className={s.profileStatus}>
+                    {props.profile.aboutMe}
+                </div>
+                <div className={s.profileJob}>
+                    {props.profile.lookingForAJobDescription}
+                </div>
+                <div className={s.profileJob}>
+                    {props.profile.contacts.vk}
+                </div>
+            </div>
         </div>
     </div >
 };
