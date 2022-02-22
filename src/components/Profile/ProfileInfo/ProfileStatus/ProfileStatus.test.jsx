a53d5f534c704267b5bcb301c9f02e66bf4be3a3
rojinald <rodjinals@gmail.com>
2022-02-22 20:21:09 +0300

    tests ProfileStatus

---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+
+describe("ProfileStatus component", () => {
+   test("status from props should be in the state", () => {
+      const component = create(<ProfileStatus status="rojinald" />);
+      const instance = component.root;
+      expect(instance.props.status).toBe("rojinald");
+   });
+   test("after creation span should be displayed", () => {
+      const component = create(<ProfileStatus status='rojinald' />);
+      const root = component.root;
+      let span = root.findByType("span")
+      expect(span).not.toBeNull();
+   });
+   test("after creation span should contains correct status", () => {
+      const component = create(<ProfileStatus status='rojinald' />);
+      const root = component.root;
+      let span = root.findByType("span")
+      expect(span.children[0]).toBe("rojinald");
+   });
+   test("after creation input shouldnt be displayed", () => {
+      const component = create(<ProfileStatus status='rojinald' />);
+      const root = component.root;
+      expect(() => {
+         let input = root.findByType("input")
+      }).toThrow();
+   });
+
+   test("input should be displayed in editMode instead of span", () => {
+      const component = create(<ProfileStatus status='rojinald' />);
+      const root = component.root;
+      let span = root.findByType("span")
+      span.props.onDoubleClick()
+      let input = root.findByType("input")
+      expect(input.props.value).toBe("rojinald");
+   });
+});
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import authReducer from "./auth-reducer";
 import dialogReducer from "./dialog-reducer";
 import friendsSideBarReducer from "./friendsSideBar-reducer";
@@ -17,7 +17,8 @@ let reducers = combineReducers({
    app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 window.store = store;
 
